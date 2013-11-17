8519c88b7ec94532eb32c373a3600611a42a509a
Walkman <kissgyorgy@me.com>
2013-11-17 02:18:10 +0100

    Added models, modified postgresql backend, example working
    * Added BaseTenant model, implemented necessary backend features.
    * Cannot get connection if schema is not set!
    Schema initially set to None, so in case of any trouble where the schema for some reason
    couldn't set, an Exception will be thrown instead of messing with the public schema.
    * Added a working example, debug_toolbar required.
    * Added two signals. The database should be synced in a task queue
    after the post_schema_create signal sent.

--- tenants/backends/postgresql/base.py
+++ tenants/backends/postgresql/base.py
@@ -1,8 +1,14 @@
+import re
 from django.conf import settings
 from django.db.backends.postgresql_psycopg2 import base
 
 
+# from the postgresql doc
+# http://www.postgresql.org/docs/9.1/static/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS
+SQL_IDENTIFIER_PATTERN = re.compile('^[_a-zA-Z][_a-zA-Z0-9]{,62}$')
+
 PUBLIC_SCHEMA = getattr(settings, 'PUBLIC_SCHEMA', 'public')
+
 DatabaseError = base.DatabaseError
 IntegrityError = base.IntegrityError
 
@@ -10,12 +16,13 @@ IntegrityError = base.IntegrityError
 class DatabaseWrapper(base.DatabaseWrapper):
     def __init__(self, *args, **kwargs):
         super(DatabaseWrapper, self).__init__(*args, **kwargs)
-        self._schema = PUBLIC_SCHEMA
+        self._schema = None
 
     def init_connection_state(self):
         super(DatabaseWrapper, self).init_connection_state()
 
         # For explanation: https://code.djangoproject.com/ticket/21453
+        # This method is in transaction mode (PEP-249)
         self.set_autocommit(True)
 
         # Do it in every new connection, not in every cursor() request,
@@ -25,19 +32,48 @@ class DatabaseWrapper(base.DatabaseWrapper):
         # PEP-249, also respect database setting
         self.set_autocommit(False)
 
+    def set_schema(self, schema):
+        """Set schema and modify search_path accordingly."""
+        self._schema = schema
+        self._set_search_path()
+
+    def get_schema(self):
+        """Get currently active database schema."""
+        return self._schema
+
+    @staticmethod
+    def schema_valid(identifier):
+        return SQL_IDENTIFIER_PATTERN.match(identifier)
+
+    def schema_exists(self, schema):
+        """
+        Check if this schema already exists in the db.
+
+        Return True or False
+        """
+        cursor = self.cursor()
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM information_schema.schemata "
+                       "WHERE schema_name = %s)", [schema])
+        return cursor.fetchone()[0]
+
+    def create_schema(self, schema):
+        self.cursor().execute('CREATE SCHEMA %s', [schema])
+
+    def drop_schema(self, schema):
+        """DROP schema with EVERY tables."""
+        self.cursor().execute('DROP SCHEMA %s CASCADE', [schema])
+
     def _set_search_path(self):
         if not self._schema:
+            # The connection will get reused, so make sure
+            # we don't allow connecting until schema is set!
+            self.connection = None
             raise RuntimeError('Schema is not set on connection')
+
         # Database will search in schema from left to right when
         # looking for the object (table, index, sequence, etc.)
         # so if something exists on the tenant schema, it will found that first
         if self._schema == PUBLIC_SCHEMA:
-            self.connection.cursor().execute('SET search_path TO %s', [PUBLIC_SCHEMA])
+            self.cursor().execute('SET search_path TO %s', [PUBLIC_SCHEMA])
         else:
-            self.connection.cursor().execute('SET search_path TO %s,%s', [self._schema, PUBLIC_SCHEMA])
-
-    def set_schema(self, schema):
-        self._schema = schema
-
-    def get_schema(self):
-        return self._schema
+            self.cursor().execute('SET search_path TO %s,%s', [self._schema, PUBLIC_SCHEMA])
---
+++ tenants/models.py
@@ -0,0 +1,52 @@
+from django.db import models, connection
+from django.core.exceptions import PermissionDenied, ValidationError
+from tenants.backends.postgresql.base import PUBLIC_SCHEMA, IntegrityError
+from tenants.signals import post_schema_create, pre_schema_delete
+
+
+class BaseTenant(models.Model):
+    domain = models.CharField(max_length=128, unique=True,
+                              help_text='Domain name without www and port')
+
+    schema = models.CharField(max_length=63, unique=True,
+                              help_text='Database schema name')
+
+    class Meta:
+        abstract = True
+
+    def save(self, create_schema=True, *args, **kwargs):
+        """
+        Save the tenant instance in the database after creating the related database schema.
+        """
+        is_new = self.pk is None
+        if is_new and connection.get_schema() not in (self.schema, PUBLIC_SCHEMA):
+            raise PermissionDenied("Can't update tenant outside it's own schema or the public schema. "
+                                   "Current schema is %s." % connection.get_schema())
+
+        if is_new and self.auto_create_schema:
+            if not connection.schema_valid(self.schema):
+                raise ValidationError('Invalid string used for the schema name %s.' % self.schema)
+
+            if connection.schema_exists(self.schema):
+                raise IntegrityError('Schema %s already exists' % self.schema)
+            # CREATE schema in the database, but not actually sync the tables for it
+            # kind of "reservation" for the name. Put the syncdb process in a worker thread!
+            connection.create_schema(self.schema)
+            post_schema_create.send(sender=BaseTenant, tenant=self)
+
+        # only save tenant instance in the database if everything was ok
+        super(BaseTenant, self).save(*args, **kwargs)
+
+    def delete(self, drop_schema=False, *args, **kwargs):
+        """
+        Delete the tenant instance from the database after dropping the related database schema.
+        """
+        if connection.get_schema() not in (self.schema, PUBLIC_SCHEMA):
+            raise PermissionDenied("Can't delete tenant outside it's own schema or the public schema. "
+                                   "Current schema is %s." % connection.get_schema())
+
+        if self.auto_drop_schema and connection.schema_exists(self.schema):
+            pre_schema_delete.send(sender=BaseTenant, tenant=self)
+            connection.drop_schema(self.schema)
+
+        super(BaseTenant, self).delete(*args, **kwargs)
